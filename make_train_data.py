649971d504e996720dba95989d5130458f1c1ffa
shi3z <shi3z@me.com>
2015-07-09 08:12:11 +0900

    fix for mac

--- make_train_data.py
+++ make_train_data.py
@@ -3,10 +3,12 @@ import commands
 import subprocess
 
 def cmd(cmd):
-	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	p.wait()
-	stdout, stderr = p.communicate()
-	return stdout.rstrip()
+	return commands.getoutput(cmd)
+#   Macだとsubprocessがうまく動かないので推奨されてないけどcommands.getoutputで
+#	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#	p.wait()
+#	stdout, stderr = p.communicate()
+#	return stdout.rstrip()
 
 #labels
 dirs = cmd("ls "+sys.argv[1])
@@ -29,18 +31,17 @@ for label in labels:
 	workdir = pwd+"/"+sys.argv[1]+"/"+label
 	imageFiles = cmd("ls "+workdir+"/*.jpg")
 	images = imageFiles.splitlines()
-	
+	print(label)
 	labelsTxt.write(label+"\n")
 	startCnt=cnt
 	length = len(images)
 	for image in images:
 		imagepath = imageDir+"/image%07d" %cnt +".jpg"
-		cmd("cp "+workdir+"/"+image+" "+imagepath)
+		cmd("cp "+image+" "+imagepath)
 		if cnt-startCnt < length*0.75:
 			train.write(imagepath+" %d\n" % classNo)
 		else:
 			test.write(imagepath+" %d\n" % classNo)
-		print imagepath 
 		cnt += 1
 	
 	classNo += 1
